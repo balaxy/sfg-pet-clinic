3c4d438eeddb3528923aac39e7c377ae39e09a0c
balaxy <balaxy@bitbucket.org>
2018-10-09 20:21:45 +0200

    Add base services + index controller

--- pet-clinic-data/src/main/java/de/pandi/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/de/pandi/sfgpetclinic/services/PetService.java
@@ -1,14 +1,6 @@
 package de.pandi.sfgpetclinic.services;
 
-import de.pandi.sfgpetclinic.model.Owner;
 import de.pandi.sfgpetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-    Pet findById(Long id);
-
-    Pet save(Owner owner);
-
-    Set<Pet> findAll();
+public interface PetService extends  CrudService<Pet, Long>{
 }
--- pet-clinic-data/src/main/java/de/pandi/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/de/pandi/sfgpetclinic/services/VetService.java
@@ -2,12 +2,5 @@ package de.pandi.sfgpetclinic.services;
 
 import de.pandi.sfgpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-    Vet findById(Long id);
-
-    Vet save(Vet owner);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long>{
 }
